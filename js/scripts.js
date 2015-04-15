87457b1a1e4b57e60f9f0d96ddc7c27dc98bf4b9
Jonathan Lin & Conor Baumgart <student@epicodus.com>
2015-04-15 10:13:20 -0700

    Add functionality to website

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,27 @@
 var bankAccount = {
     balance: 0,
     deposit: function(amount) {
-        this.balance += amount;
+        this.balance += parseInt(amount);
     },
     withdraw: function(amount) {
-        this.balance -= amount;
+        this.balance -= parseInt(amount);
     }
 };
+
+
+$(function() {
+    $('form#add').submit(function(event) {
+        event.preventDefault();
+        var name = $('input#name').val();
+        var initial_Deposit = $('input#initial_Deposit').val();
+        var bankAccount1 = Object.create(bankAccount);
+            bankAccount1.deposit(initial_Deposit);
+
+
+
+        $('.final_balance').text(bankAccount1.balance);
+
+
+
+    });
+});
